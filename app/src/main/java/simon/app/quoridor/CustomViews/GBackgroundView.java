74cbaec1cf46be5ab7a207569d5727e5b4b3cc39
Simon Martineau <simon.martineau.3@ulaval.ca>
2020-03-31 23:57:48 -0400

    Created GBackgroundView

--- app/src/main/java/simon/app/quoridor/CustomViews/GBackgroundView.java
+++ app/src/main/java/simon/app/quoridor/CustomViews/GBackgroundView.java
@@ -1,26 +1,32 @@
 package simon.app.quoridor.CustomViews;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
 import simon.app.quoridor.Core.WindowView;
 
 public class GBackgroundView extends GView {
-	public GBackgroundView(WindowView windowView, int x, int y) {
+	private Bitmap mBitmap;
+
+	public GBackgroundView(WindowView windowView, int x, int y, Bitmap bitmap) {
 		super(windowView, x, y);
+
+		mBitmap = bitmap;
+		setZIndex(-100);
 	}
 
 	@Override
 	public int getWidth() {
-		return 0;
+		return mBitmap.getWidth();
 	}
 
 	@Override
 	public int getHeight() {
-		return 0;
+		return mBitmap.getHeight();
 	}
 
 	@Override
 	public void draw(Canvas canvas) {
-
+		canvas.drawBitmap(mBitmap, 0, 0, null);
 	}
 }
